0e97426f5d666bbd00b01cf02e2430ec57c1e810
Steve Temple <steve@gibedigital.com>
2021-11-16 14:02:20 +0000

    WIP v48

---
+++ MultiFacetLucene4/FacetFieldInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MultiFacetLucene
+{
+	public class FacetFieldInfo
+	{
+		public FacetFieldInfo()
+		{
+			Selections = new List<string>();
+			Ranges = new List<Range>();
+			MaxToFetchExcludingSelections = 20;
+		}
+		public string FieldName { get; set; }
+		public List<string> Selections { get; set; }
+		public int MaxToFetchExcludingSelections { get; set; }
+		public bool IsRange { get; set; }
+		public List<Range> Ranges { get; set; }
+
+		public virtual bool HasSelections
+		{
+			get
+			{
+				return Selections.Any();
+			}
+		}
+	}
+
+	public class Range
+	{
+		public string Id { get; set; }
+		public string From { get; set; }
+		public string To { get; set; }
+	}
+
+
+}
---
+++ MultiFacetLucene4/IFacetBitSetCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lucene.Net.Index;
+using Lucene.Net.Util;
+using MultiFacetLucene.Configuration;
+
+namespace MultiFacetLucene
+{
+	public interface IFacetBitSetCalculator
+	{
+		IEnumerable<FacetSearcher.FacetValues.FacetValueBitSet> GetFacetValueBitSets(IndexReader indexReader, FacetFieldInfo info);
+		OpenBitSetDISI GetFacetBitSet(IndexReader indexReader, FacetFieldInfo info, string value);
+	}
+}
